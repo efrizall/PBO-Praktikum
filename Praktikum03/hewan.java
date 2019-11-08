8d0aee0b8848d244a8799a19298933d01bad1e3d
Efrizal <43870118+efrizall@users.noreply.github.com>
2019-11-08 16:40:07 +0700

    Praktikum 3

---
+++ Praktikum03/hewan.java
@@ -0,0 +1,14 @@
+public class hewan{
+    String jenis;
+    int kaki;
+    String warna;
+    String habitat;
+
+    public void suara(String suara){
+        System.out.println(suara);
+    }
+
+    public String makanan(String makan){
+        return makan;
+    }
+}
